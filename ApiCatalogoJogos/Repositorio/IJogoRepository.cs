ad493794f2f094a4f9f798ecd89f9307ca2f837c
Vinicius Ribeiro <viniciusribeiro113@gmail.com>
2021-09-10 13:39:48 -0300

    Primeiro commit

---
+++ ApiCatalogoJogos/Repositorio/IJogoRepository.cs
@@ -0,0 +1,19 @@
+﻿using ApiCatalogoJogos.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCatalogoJogos.Repositorio
+{
+    public interface IJogoRepository
+    {
+        Task<List<Jogo>> Obter(int pagina, int quantidade);
+        Task<Jogo> Obter(Guid id);
+        Task<List<Jogo>> Obter(string nome, string produtora);
+        Task Inserir(Jogo jogo);
+        Task Atualizar(Jogo jogo);
+        Task Remover(Guid id);
+    }
+    
+}
